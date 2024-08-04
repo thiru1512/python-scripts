cf21c3347cc7f2217ad8d6a51befa1523c11afb3
Your Name <you@example.com>
2024-08-04 21:35:28 +0530

    new

---
+++ strings/for.py
@@ -0,0 +1,12 @@
+for i in range(1,10):
+    for j in range(1,10):
+        sum = i + j
+        if sum % 2 == 0:
+            print("0",end="")
+        else:
+            print("1",end="")
+    else:
+        print("")
+
+
+    
