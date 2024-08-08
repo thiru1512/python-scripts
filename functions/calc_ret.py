8d72bd65287d5015ba3dc634f2a26821a78cb44f
Your Name <you@example.com>
2024-08-08 13:57:32 +0530

    new

--- functions/calc_ret.py
+++ functions/calc_ret.py
@@ -6,7 +6,12 @@ def sub(num1,num2):
     subtraction = num1 - num2
     return subtraction
 
+def mul(num1,num2):
+    multiply = num1 * num2
+    return multiply
+
 
 print("The sum of two numbers",add(5,10))
 print("The subtraction of two numbers",sub(10,2))
+
     
--- functions/mod.py
+++ functions/mod.py
@@ -1,2 +1,10 @@
 import calc_ret as calculator
-calculator
+
+sum = calculator.add(5,2)
+sub = calculator.sub(5,2)
+mul = calculator.mul(5,2)
+print(sum,sub,mul)
+
+
+
+
